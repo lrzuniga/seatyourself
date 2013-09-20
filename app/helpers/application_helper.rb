1efd1e971b0fef8eaf4cff4610865fd969b57061
dfry22 <davidfreiman@gmail.com>
2013-09-20 14:53:45 -0400

    actually got timeslots method to work and now displaying forms with a helping of epicness

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,22 +1,5 @@
 module ApplicationHelper
 
-		def foo(start_time)
-		timeslots = []	
-		a_time = start_time
-		reservation_interval = 30.minutes
-
-		begin
-			timeslots << a_time
-			a_time += reservation_interval
-
-			if (a_time + reservation_interval) > a_time.change(hour: closing_hour)
-				a_time = (a_time + 1.day).change(hour: opening_hour)
-			end 
-
-		end while a_time < end_time	
-
-		timeslots
-	end	
 
 
 end
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -10,4 +10,28 @@ class Restaurant < ActiveRecord::Base
 		:numericality => { :only_integer => true, :greater_than => :open_hour }	
 
 
+	def timeslots
+		timeslots = []
+		if self.open_hour < 12
+			a_time_intermediate = self.open_hour.to_s + "am"
+		else 	
+			a_time_intermediate = self.open_hour.to_s + "pm"
+		end
+
+		a_time = a_time_intermediate.to_time
+		reservation_interval = 30.minutes
+
+		begin
+			timeslots << a_time
+			a_time += reservation_interval
+
+			if a_time > a_time.change(hour: close_hour)
+				a_time += 1.day
+				a_time = a_time.change(hour: open_hour)
+			end	
+
+		end while a_time < Time.zone.now + 3.days
+		timeslots
+	end	
+
 end
